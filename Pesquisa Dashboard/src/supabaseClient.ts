2c3276516f58e189f9864faa8902d67f55fc90d3
adrianonovamanancial-lab <adriano.novamanancial@gmail.com>
2026-03-16 07:55:15 -0300

    Update supabaseClient.ts

--- Pesquisa Dashboard/src/supabaseClient.ts
+++ Pesquisa Dashboard/src/supabaseClient.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://ofwppcrrvnuweixckcqap.supabase.co"
-const supabaseAnonKey = "sb_publishable_r2Nr6Fzju0G-7J1Qgt8cdw_dwD7hQjS"
+const supabaseUrl = "https://ofwppcrvnuwexickcqap.supabase.co"
+const supabaseKey = "sb_publishable_r2Nr6Fzju0G-7J1Qgt8cdw_dwD7hQjS"
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
